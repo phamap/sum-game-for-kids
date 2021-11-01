513cb9c9f8f1d7efc48d48f04c666eb8b0b4627c
Aleksey Milov <bit31337@gmail.com>
2021-11-01 12:04:01 +0700

    + fox

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 (() => {
-  const max = 10;
-  const min = 2;
+  const max = 25;
+  const min = 3;
+
   const firsHtmlNum = document.querySelector('#firstNum');
   const secondHtmlNum = document.querySelector('#secondNum');
   const htmlMessage = document.querySelector('#message');
   const htmlRes = document.querySelector('#resList');
   const htmlExercise = document.querySelector('.exercise');
-  const htmlImg = document.querySelector('.img');
-  const input = document.querySelector('input');
+  const htmlImg = document.querySelector('.minotaur');
+  const input = document.querySelector('#number');
+  const button = document.querySelector('#answer');
+  const sign = document.querySelector('#sign');
+
   const messages = {
     success: 'Поздравляю, жалкий монстр был побежден! Тебе нет равных!',
     error: 'Тебе не хватило совсем чуть-чуть. В следующий раз ты обязательно справишься!'
@@ -22,18 +26,26 @@
   let attackInterval, idleInterval, dieInterval, bossWinInterval;
 
   idleInterval = setInterval(() => {
-    htmlImg.style.backgroundPosition = `-${sum}px -270px`;
+    htmlImg.style.backgroundPosition = `-${sum}px 10px`;
     sum += increment;
-    if (sum > 1600) {
+    if (sum > 1300) {
       sum = 40;
     }
-  }, 100);
+  }, 150);
 
   generateRandom();
 
-  input.addEventListener('keydown', (e) => {
+  button.addEventListener('click', () => {
+    onAnswer();
+  });
+
+  input.oninput = function() {
+    console.log('Function: oninput: ', input.value)
+  };
+
+  function onAnswer() {
     clearInterval(idleInterval);
-    if (!attackInterval) {
+    /*if (!attackInterval) {
       attackInterval = setInterval(() => {
         htmlImg.style.backgroundPosition = `-${attackSum}px -840px`;
         attackSum += increment;
@@ -41,44 +53,43 @@
           attackSum = 10;
         }
       }, 100);
-    }
-    if(e.key === 'Enter') {
-      results.push({
-        exercise: `${firstNum} + ${secondNum} = ${e.target.value}`,
-        res: checkExercise(firstNum, secondNum, +e.target.value)
-      });
+    }*/
 
-      if (results.length < 20) {
-        generateRandom();
+    results.push({
+      exercise: `${firstNum} + ${secondNum} = ${input.value}`,
+      res: checkExercise(firstNum, secondNum, +input.value)
+    });
+
+    if (results.length < 20) {
+      generateRandom();
+    } else {
+      clearInterval(attackInterval);
+      htmlExercise.classList.add('hide');
+
+      if (checkResult(results)) {
+        htmlMessage.innerHTML = messages.success;
+        htmlImg.classList.add('lose');
+        dieInterval = setInterval(() => {
+          htmlImg.style.backgroundPosition = `-${sum}px -2530px`;
+          sum += increment;
+          if (sum > 1600) {
+            clearInterval(dieInterval);
+          }
+        }, 100);
       } else {
-        clearInterval(attackInterval);
-        htmlExercise.classList.add('hide');
-
-        if (checkResult(results)) {
-          htmlMessage.innerHTML = messages.success;
-          htmlImg.classList.add('lose');
-          dieInterval = setInterval(() => {
-            htmlImg.style.backgroundPosition = `-${sum}px -2530px`;
-            sum += increment;
-            if (sum > 1600) {
-              clearInterval(dieInterval);
-            }
-          }, 100);
-        } else {
-          htmlMessage.innerHTML = messages.error;
-          htmlImg.classList.add('win');
-          bossWinInterval = setInterval(() => {
-            htmlImg.style.backgroundPosition = `-${sum}px -1400px`;
-            sum += increment;
-            if (sum > 1600) {
-              sum = 40;
-            }
-          }, 100);
-        }
-        updateResults(results);
+        htmlMessage.innerHTML = messages.error;
+        htmlImg.classList.add('win');
+        bossWinInterval = setInterval(() => {
+          htmlImg.style.backgroundPosition = `-${sum}px -1400px`;
+          sum += increment;
+          if (sum > 1600) {
+            sum = 40;
+          }
+        }, 100);
       }
+      updateResults(results);
     }
-  });
+  }
 
   function updateResults(results) {
     const el = document.createElement('ul');
@@ -103,7 +114,7 @@
   function generateRandom() {
     input.value = 0;
     firstNum = getRandomInt(min, max);
-    secondNum = getRandomInt(min, max - firstNum);
+    secondNum = max - firstNum > min ? getRandomInt(min, max - firstNum) : max - firstNum;
     firsHtmlNum.innerHTML = firstNum;
     secondHtmlNum.innerHTML = secondNum;
   }
